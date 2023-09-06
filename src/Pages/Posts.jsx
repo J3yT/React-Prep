3642e5fe62fd7aa9a340256383850694d160d926
Jason Tavera <taverajason7@gmail.com>
2023-09-05 22:44:13 -0500

    added the usestate hook and created useStates for searching posts, loading state, and searching by id, also created an input button that fetches data upon click

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,19 +1,60 @@
-import React, { useEffect } from 'react'
-import { useParams } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function Posts() {
-    const { id } = useParams();
-    
-    useEffect(() => {
-      async function fetchPosts() {
-        const data = await fetch('https://jsonplaceholder.typicode.com/posts/1')
-        console.log(data)
-      }
-      fetchPosts()
-    }, [])
-    return (
-    <div>
-      {id}
-    </div>
-  )
+  const { id } = useParams();
+  const [posts, SetPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchId, setSearchId] = useState(id);
+
+  function onSearch() {
+    fetchPosts(searchId);
+  }
+
+  async function fetchPosts(userId) {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`
+    );
+    SetPosts(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      <div className="post__search">
+        <button>← Back</button>
+        <div className="post__search--container">
+          <label className="post__search--label">Search by Id</label>
+          <input
+            type="number"
+            value={searchId}
+            onChange={(event) => setSearchId(event.target.value)}
+          />
+          <button onClick={() => onSearch()}>Enter</button>
+        </div>
+      </div>
+      {loading
+        ? new Array(10).fill(0).map((_, index) => (
+            <div className="post" key={index}>
+              <div className="post__title">
+                <div className="post__title--skeleton"></div>
+              </div>
+              <div className="post__body">
+                <p className="post__body--skeleton"></p>
+              </div>
+            </div>
+          ))
+        : posts.map((post) => (
+            <div className="post" key={post.id}>
+              <div className="post__title">{post.title}</div>
+              <p className="post__body">{post.body}</p>
+            </div>
+          ))}
+    </>
+  );
 }
