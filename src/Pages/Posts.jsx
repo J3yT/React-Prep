9d4083a4df92dbd99d471b600cc945e42ca58d70
Jason Tavera <taverajason7@gmail.com>
2023-09-05 19:16:34 -0500

    installed axios dependency and created a async function that awaits a fetch containing json placeholder data

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from "react-router-dom"
 
 export default function Posts() {
-  return (
-    <div>Posts</div>
+    const { id } = useParams();
+    
+    useEffect(() => {
+      async function fetchPosts() {
+        const data = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+        console.log(data)
+      }
+      fetchPosts()
+    }, [])
+    return (
+    <div>
+      {id}
+    </div>
   )
 }
